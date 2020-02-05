5adca5f6106b8d086c471a7869620685cc593a21
JhongHawan <brianjhong@outlook.com>
2020-02-05 11:09:25 -0800

    Build folder public for gh pages

---
+++ landingpage/build/precache-manifest.0557c1521779c02901aa4996d4cbfa4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72766df5c6b992e632bce68d7222e140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "532a1e83d76cfd7996d5",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "a686601b9d75d2e22a3b",
+    "url": "/static/js/2.874bde36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.874bde36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532a1e83d76cfd7996d5",
+    "url": "/static/js/main.daf194fe.chunk.js"
+  },
+  {
+    "revision": "1c9782f903e61d9a32c2",
+    "url": "/static/js/runtime-main.28e97d5f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
