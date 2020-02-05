a715b964889c1712fb6c32a998e30c5ac1b79363
JhongHawan <brianjhong@outlook.com>
2020-02-05 11:57:34 -0800

    Updates

---
+++ precache-manifest.a3027f06c0bcc26cdefc0c668b122e5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fbdfb75a89fd44ac428330dcf0d90e",
+    "url": "/JhongHawan/InfoCapstone2020.git/index.html"
+  },
+  {
+    "revision": "9b72a264ac8de76fabb6",
+    "url": "/JhongHawan/InfoCapstone2020.git/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "a686601b9d75d2e22a3b",
+    "url": "/JhongHawan/InfoCapstone2020.git/static/js/2.874bde36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/JhongHawan/InfoCapstone2020.git/static/js/2.874bde36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b72a264ac8de76fabb6",
+    "url": "/JhongHawan/InfoCapstone2020.git/static/js/main.5a81391f.chunk.js"
+  },
+  {
+    "revision": "9b4437e8a476d3caa45a",
+    "url": "/JhongHawan/InfoCapstone2020.git/static/js/runtime-main.88cefe15.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/JhongHawan/InfoCapstone2020.git/static/media/logo.ee7cd8ed.svg"
+  }
+]);
