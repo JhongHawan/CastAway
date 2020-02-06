ed9d1d99648369de7dff9c20b6503a2acec2e62b
Brian Jhong <brianjhong@outlook.com>
2020-02-06 12:56:56 -0800

    Updates

---
+++ precache-manifest.360e485d7a3c44ddfa20e85bb8c6a6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507d6c226334d3ede7166b9bdfb5bc0b",
+    "url": "/InfoCapstone2020/index.html"
+  },
+  {
+    "revision": "b366709ac7a4d0108adb",
+    "url": "/InfoCapstone2020/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "a686601b9d75d2e22a3b",
+    "url": "/InfoCapstone2020/static/js/2.874bde36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/InfoCapstone2020/static/js/2.874bde36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b366709ac7a4d0108adb",
+    "url": "/InfoCapstone2020/static/js/main.5af168fa.chunk.js"
+  },
+  {
+    "revision": "dea25f9f9737729a2cfb",
+    "url": "/InfoCapstone2020/static/js/runtime-main.ff84cdf5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/InfoCapstone2020/static/media/logo.ee7cd8ed.svg"
+  }
+]);
