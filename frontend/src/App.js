d55eb7c95f057858f0bbfc8de4b37006dca655f0
Brian Jhong <brianjhong@outlook.com>
2020-02-06 23:29:18 -0800

    Finished basic structure of the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Homepage for our project
         </a>
       </header>
     </div>
---
+++ frontend/src/components/Context.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Context() {
+  return(
+   <div className="Context">
+     <Navbar></Navbar>
+     <h1>Context Page</h1>
+   </div>
+  );
+}
+
+export default Context; 
