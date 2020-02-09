d495fffa9b761babd4937c3b551bdd3b9340ed15
JhongHawan <brianjhong@outlook.com>
2020-02-09 01:32:54 -0800

    Installed material-ui

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import AppNavBar from './components/AppNavBar'; 
+import Template from './components/Template';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Our landing page INFO 2020 Capstone. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppNavBar></AppNavBar>
+      <Template></Template>
     </div>
   );
 }
