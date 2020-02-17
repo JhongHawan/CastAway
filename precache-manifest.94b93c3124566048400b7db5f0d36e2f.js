f913a2cb8eb25d1e022308d1c8c21a836b5679e0
Brian Jhong <brianjhong@outlook.com>
2020-02-16 18:55:36 -0800

    Updates

---
+++ precache-manifest.94b93c3124566048400b7db5f0d36e2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93370c6b924406a1f3ca93035990fa8f",
+    "url": "/InfoCapstone2020/index.html"
+  },
+  {
+    "revision": "c16c55be4d55e010467a",
+    "url": "/InfoCapstone2020/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "2ca9efab00de01f1217d",
+    "url": "/InfoCapstone2020/static/js/2.a2504ee6.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/InfoCapstone2020/static/js/2.a2504ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16c55be4d55e010467a",
+    "url": "/InfoCapstone2020/static/js/main.0083b496.chunk.js"
+  },
+  {
+    "revision": "dea25f9f9737729a2cfb",
+    "url": "/InfoCapstone2020/static/js/runtime-main.ff84cdf5.js"
+  },
+  {
+    "revision": "4010d615ebc0d4abc0ef6e2b24b9bdc0",
+    "url": "/InfoCapstone2020/static/media/landing.4010d615.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/InfoCapstone2020/precache-manifest.6852a94f23c2fa80fabcc3bb1a62218c.js"
+  "/InfoCapstone2020/precache-manifest.94b93c3124566048400b7db5f0d36e2f.js"
 );
 
 self.addEventListener('message', (event) => {
