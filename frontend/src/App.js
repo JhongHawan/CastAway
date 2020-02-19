756be49d702bbab712380ccfbcbb2331ec93288c
Tracy Huynh <thuynh12@uw.edu>
2020-02-18 20:58:42 -0800

    added logo to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import AppNavBar from './components/AppNavBar';
 import Context from './components/Context';
 import Stories from './components/Stories';
-import Visualization from './components/Visualization'; 
+import Visualization from './components/Visualization';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
+
+import ReactFullpage from '@fullpage/react-fullpage';
+import { Box } from '@material-ui/core';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar/>
+        <NavBar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/context" component={Context} />
@@ -20,14 +23,52 @@ function App() {
         </Switch>
       </div>
     </Router>
-    
+
   );
 }
 
 const Home = () => (
+  // <ReactFullpage
   <div>
     <h1>Home Page</h1>
   </div>
+  // <FullpageQuotes />
 );
+
+class FullpageQuotes extends React.Component {
+  // Good to knows
+  onLeave(origin, destination, direction) {
+    console.log("Leaving section " + origin.index);
+  }
+  afterLoad(origin, destination, direction) {
+    console.log("After load: " + destination.index);
+  }
+  render() {
+    return(
+      // <ReactFullpage></ReactFullpage> 
+      <ReactFullpage 
+        scrollOverflow={true}
+        sectionsColor={['#F0F5F9']}
+        onLeave={this.onLeave.bind(this)}
+        afterLoad={this.afterLoad.bind(this)}
+        render={({ state, fullpageApi}) => {
+          return(
+            <Box id="fullpage-wrapper">
+              <Box className="section quote-section">
+                <h1 className="lightTest">CAST| |AWAY</h1>
+                <h1 className="regTest">CAST| |AWAY</h1>
+                <h1 className="boldTest">CAST| |AWAY</h1>
+              </Box>
+              <Box className="section">
+                <h1>BOO</h1>
+              </Box>
+            </Box>
+          );
+        }}
+      />
+    )
+  }
+}
+
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,14 +5,24 @@ import {
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Logo from './Logo'; 
+import Logo from './Logo';
+import '../App.css';
+import { Grid, Typography } from '@material-ui/core';
 
 function NavBar() {
-  return(
-    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="transparent">
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Brand className="ml-auto" as={Link} to="/">
-          <Logo className="logo" />
+        {/* <Logo className="logo" /> */}
+        <Grid container justify="center" alignItems="center" spacing={1}> 
+          <Grid item align="center">
+            <img src="castawayLogo.png" width="50px" alt="castaway boat logo" />
+          </Grid>
+          <Grid item align="center">
+            <h1 className="brand">CAST| |AWAY</h1>
+          </Grid>
+        </Grid>
       </Navbar.Brand>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
@@ -22,9 +32,9 @@ function NavBar() {
           <Nav.Link as={Link} to="/visualization">Visualization</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-      
     </Navbar>
   );
 }
+
 
 export default NavBar; 
