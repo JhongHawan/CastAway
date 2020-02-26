ecb0efd34b52daa3b0f83d47d70f98064db832d3
Tracy Huynh <thuynh12@uw.edu>
2020-02-25 16:43:43 -0800

    adding image comparer

--- frontend/src/components/Context.js
+++ frontend/src/components/Context.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Grid, Box } from '@material-ui/core';
+import ReactCompareImage from 'react-compare-image';
+
+// images
+// import refugeeDef from '../../public/refugee-edit.png';
+// import immigrantDef from '../../public/immigrant-edit.png';
 
 function Context() {
-  return(
-   <div className="Context">
-     <h1>Context Page</h1>
-   </div>
+  return (
+    <div>
+      <Box>
+        <Box className="sectionGradient">
+          <Box className="soloHeading">
+            <h1 className="bigTitle titleCenter">CONTEXT</h1>
+          </Box>
+        </Box>
+        <Box style={{ textAlign: "center" }}>
+          <Box style={{ maxWidth: "800px", margin: "auto"}}>
+            <ReactCompareImage leftImage="refugee-edit.png" rightImage="immigrant-edit.png" />
+          </Box>
+        </Box>
+      </Box>
+
+    </div>
   );
 }
 
