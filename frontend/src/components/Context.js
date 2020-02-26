6b4ac689fafb8ab7817624fa005745d45b9c6e34
Tracy Huynh <thuynh12@uw.edu>
2020-02-25 16:59:44 -0800

    added context cards

--- frontend/src/components/Context.js
+++ frontend/src/components/Context.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Grid, Box } from '@material-ui/core';
 import ReactCompareImage from 'react-compare-image';
+import ContextCard from './ContextCard';
 
-// images
-// import refugeeDef from '../../public/refugee-edit.png';
-// import immigrantDef from '../../public/immigrant-edit.png';
 
 function Context() {
   return (
@@ -16,9 +14,36 @@ function Context() {
           </Box>
         </Box>
         <Box style={{ textAlign: "center" }}>
-          <Box style={{ maxWidth: "800px", margin: "auto"}}>
+          {/* <Box style={{ maxWidth: "800px", margin: "auto"}}> */}
+          <Box>
             <ReactCompareImage leftImage="refugee-edit.png" rightImage="immigrant-edit.png" />
           </Box>
+        </Box>
+        <Box className="sectionGradient">
+          <Box className="soloHeading">
+            <h2 className="bigTitle titleCenter">STATE OF THE WORLD</h2>
+          </Box>
+        </Box>
+        <Box className="heading1">
+          This is blank. Add to me plz.
+        </Box>
+        <Box className="sectionGradient">
+          <Box className="soloHeading">
+            <h2 className="bigTitle titleCenter">MYTHS</h2>
+          </Box>
+        </Box>
+        <Box className="heading1">
+          <Grid container spacing={2}>
+            <Grid item sm={4}>
+              <ContextCard />
+            </Grid>
+            <Grid item sm={4}>
+              <ContextCard />
+            </Grid>
+            <Grid item sm={4}>
+              <ContextCard />
+            </Grid>
+          </Grid>
         </Box>
       </Box>
 
