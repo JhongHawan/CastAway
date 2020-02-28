f0897214a6b4fdefa3fbde8d86c11386259e076c
Tracy Huynh <thuynh12@uw.edu>
2020-02-28 14:54:22 -0800

    updated context, stories, home

---
+++ frontend/src/components/StoriesList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+
+class storiesList extends React.Component {
+    constructor(props) {
+        super(props)
+        
+    }
+
+    render() {
+        let imageProp = this.props.imgTitle
+        let detailProp = this.props.details
+        return (
+            
+        );
+    }
+}
+
+
+export default storiesList;
