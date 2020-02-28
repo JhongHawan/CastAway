d737b5199459548b70a18bf2f84910029826f9fe
Tracy Huynh <thuynh12@uw.edu>
2020-02-28 15:16:03 -0800

    updates

--- frontend/src/components/StoriesList.js
+++ frontend/src/components/StoriesList.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import GridList from '@material-ui/core/GridList';
+
 
 class storiesList extends React.Component {
     constructor(props) {
@@ -12,8 +9,8 @@ class storiesList extends React.Component {
     }
 
     render() {
-        let imageProp = this.props.imgTitle
-        let detailProp = this.props.details
+        // let imageProp = this.props.imgTitle
+        // let detailProp = this.props.details
         return (
             
         );
