395fabc803e892b0ae539751afe0a91e95d89c72
Tracy Huynh <thuynh12@uw.edu>
2020-02-29 22:13:38 -0800

    tested new section header

--- frontend/src/components/Stories.js
+++ frontend/src/components/Stories.js
@@ -10,19 +10,44 @@ import { Grid, Box } from '@material-ui/core';
 import StoryCard from './StoryCard';
 import StoriesList from './StoriesList';
 
-function Stories() {
-  return (
-    <div >
-      <Box className="sectionGradient">
-        <Box className="heading1">
-          <h1 className="bigTitle titleCenter">STORIES</h1>
-          <h6 className="titleCenter">Putting a face to the crisis.</h6>
-        </Box>
-      </Box>
-      {/* <h1>Stories Page</h1> */}
-      <StoriesList />
+import { makeStyles } from '@material-ui/core/styles';
 
-      {/* <Grid container className="heading1" spacing={2}>
+
+const useStyles = makeStyles(theme => ({
+   heroContent: {
+      // backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(3, 5, 0),
+   },
+}))
+
+function Stories() {
+   const classes = useStyles();
+
+   return (
+      <div >
+         {/* NEW HEADING TEST */}
+         {/* <Box className={classes.heroContent}>
+            <Grid container spacing={4}>
+               <Grid item sm={6}>
+                  <h1 className="storiesTitle">STORIES</h1>
+               </Grid>
+               <Grid item>
+                  <p>Amidst the politics of the crisis, we forget that there are lives behind the articles. Behind the stories of empty cities, crowded camps and overflowing borders. There are homes abandoned, families separated, lives losts, and whole stories revolutionized. </p>
+               </Grid>
+            </Grid>
+         </Box> */}
+         <Box className="sectionGradient">
+            <Box className="heading1">
+               <h1 className="bigTitle titleCenter">STORIES</h1>
+               <h6 className="titleCenter">Putting a face to the crisis.</h6>
+            </Box>
+         </Box>
+         {/* NEW STORIES LIST GRID LSIT */}
+         <StoriesList />
+
+
+         {/* OLD STORIES - GRID OF IMAGES */}
+         {/* <Grid container className="heading1" spacing={2}>
         <Grid item lg={3}>
            <StoryCard imgTitle="prof_2.svg" details="A, 29"/>
         </Grid>
@@ -50,8 +75,8 @@ function Stories() {
 
       </Grid> */}
 
-    </div>
-  );
+      </div>
+   );
 }
 
 export default Stories; 
