6f8c41f62529f9d21ff6336a66afdd54029c9c4b
Rahma Kamel <kamelr@uw.edu>
2020-04-26 13:33:40 -0700

    update on myths, added confirmed true section

--- frontend/src/components/Myths.js
+++ frontend/src/components/Myths.js
@@ -62,14 +62,8 @@ function Myths() {
     return (
       <Container>
         <Container style={{ textAlign: 'center' }}>
-          <Row>
-            <Col>
-              <Button variant="contained" onClick={handleDebunk}>Debunk</Button>
-            </Col>
-            <Col>
-              <Button variant="outlined" onClick={handleReset}>Reset</Button>
-            </Col>
-          </Row>
+          <Button style={{ margin: '1em' }} variant="contained" onClick={handleDebunk}>Reveal all Answers</Button>
+          <Button style={{ margin: '1em' }} variant="contained" onClick={handleReset}>Reset</Button>
         </Container>
         <Row>
           <Col md={3}>
@@ -79,14 +73,14 @@ function Myths() {
           </Col>
           <Col md={3}>
             <Card caption='Refugees are forced to leave their home to escape violence.' clicked={active} redGreen={green} />
-            <Card caption='All refugees live in refugee camps.' clicked={active} redGreen={red} />
+            <Card caption='It will be easier to send help back home.' clicked={active} redGreen={red} />
             <Card caption='Refugees contribute their talent, skills, and culture.' clicked={active} redGreen={green} />
 
           </Col>
           <Col md={3}>
             <Card caption='Refugees do not pay taxes, and negatively impact the economy.' clicked={active} redGreen={red} />
             <Card caption='The refugee crisis is not a new phenomenon in history.' clicked={active} redGreen={green} />
-            <Card caption='Refugees steal jobs from US citizens.' clicked={active} redGreen={red} />
+            <Card caption='All the refugees that come are adult men.' clicked={active} redGreen={red} />
           </Col>
           <Col md={3}>
             <Card caption='Refugees come from countries all over the world.' clicked={active} redGreen={green} />
@@ -98,58 +92,58 @@ function Myths() {
     )
   }
 
+  function ContextCard(props) {
+    return (
+      <Row style={{ padding: 10 }}>
+        <ExpansionPanel style={{ background: '#F2F2F2', height: '100%' }}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+          >
+            <Container>
+              <Row>
+                <Typography component='h5' variant='h5' color='primary' className={classes.factTitle}>
+                  {props.mythTitle}
+                </Typography>
+                {/* <h4 style={{ paddingBottom: '0.5em' }}>{props.mythTitle}</h4> */}
+              </Row>
+              <Row>
+                <Typography style={{ background: "#FF6464", opacity: "0.5" }} component='overline' variant='overline'>
+                  {props.trueFalse}
+                </Typography>
+                {/* <span style={{ background: "#FF6464", opacity: "0.7" }}>Confrimed FALSE</span> */}
+                <Typography component='body1' variant='body1' className={classes.factDetail}>
+                  <strong>Fact Check:</strong>{props.mythSummary}
+                </Typography>
+                {/* <p style={{ paddingTop: '1em' }}><strong>Fact Check:</strong>{props.mythSummary}</p> */}
+              </Row>
+            </Container>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography component='body1' variant='body1'>
+              {props.mythDetails}
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Row>
+    )
+  }
+
   // TODO Convert to Typography
-  function FactCheck() {
-
-    function ContextCard(props) {
-      return (
-        <Row style={{ padding: 10 }}>
-          <ExpansionPanel style={{ background: '#F2F2F2' }}>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-            >
-              <Container>
-                <Row>
-                  <Typography component='h5' variant='h5' color='primary' className={classes.factTitle}>
-                    {props.mythTitle}
-                  </Typography>
-                  {/* <h4 style={{ paddingBottom: '0.5em' }}>{props.mythTitle}</h4> */}
-                </Row>
-                <Row>
-                  <Typography style={{ background: "#FF6464", opacity: "0.5" }} component='overline' variant='overline'>
-                    Confirmed FALSE
-                  </Typography>
-                  {/* <span style={{ background: "#FF6464", opacity: "0.7" }}>Confrimed FALSE</span> */}
-                  <Typography component='body1' variant='body1' className={classes.factDetail}>
-                    <strong>Fact Check:</strong>{props.mythSummary}
-                  </Typography>
-                  {/* <p style={{ paddingTop: '1em' }}><strong>Fact Check:</strong>{props.mythSummary}</p> */}
-                </Row>
-              </Container>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography component='body1' variant='body1'>
-                {props.mythDetails}
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </Row>
-      )
-    }
-
-
+  function FactCheckFalse() {
     return (
       <Container >
         <Row>
           <Col md={6} lg={4}>
             <ContextCard
-              mythTitle="Refugees are not vetted."
-              mythSummary=" This statement is entirely false. In fact refugees are the most vetted people in the world."
-              mythDetails="There is an extensive vetting process that usually takes up to two years. This process consists of an average of 18-20  different steps that refugees must go through to come into the US. Departments that are involved in this process include the FBI, Department of Homeland Security, the National Counterterrorism Center, and much more. Certain refugees like Syrians have a longer and more extensive process, and are the most vetted refugees in the world. Learn more about the details of the vetting process here."
+              trueFalse="Confirmed FALSE"
+              mythTitle="Immigrants and refugees are the same type of person."
+              mythSummary="This statement is false, and is a dangerous yet common assumption."
+              mythDetails="Although very similar refugees and immigrsnts are very different kinds of people. A refugee is someone who has been forced to leave their country in order to escape war, prosecution, or natural disaster. They fear persecution because of their race, religion, nationality, or political opinion. On the other hand, an immigrant is An immigrant is someone who makes a conscious decision to leave his or her home and move to a foreign country with the intention of settling there."
             />
 
             <ContextCard
-              mythTitle="All refugees are terrorists."
+              trueFalse="Confirmed FALSE"
+              mythTitle="All refugees are terrorists with a violent agenda."
               mythSummary=" Refugees are fleeing from violence and terrorism, they are the victims of this kind of opression back home."
               mythDetails="When they flee to a host country they do so to find peace and to start new lives. Their lives are being threatened every day, that is why they are running away. This statement is ironic, and a hurtful generalization that often costs real people and families their lives. The extensive vetting process that the refugees go through makes sure to eliminate any viable threat of terrorism or violence. This process has been very successful so far, of the roughly three million refugees admitted since 1980, not one refugee has ever killed a single American in a terrorist attack."
             />
@@ -157,13 +151,15 @@ function Myths() {
           </Col>
           <Col md={6} lg={4}>
             <ContextCard
-              mythTitle="No other countries are taking refugees."
+              trueFalse="Confirmed FALSE"
+              mythTitle="The United States is the only country that takes refugees."
               mythSummary=" In terms of Arab refugees, Muslim countries are taking the most burden of the refugee crisis."
-              mythDetails="However, it is undeniable that the US is more politically and economically stable than any of the Arab countries combined. Oppression exists in other Arab countries, the same oppression that forced refugees to leave their own countries in the first place. Additionally, several countries in Europe have welcomed refugees to the best of their ability. In 2019 Europe accepted 123, 920 refugees and have taken even more in past years. However, it is undeniable that the US has more means and resources to take in refugees, and if possible should continue to help to the best of their ability. "
+              mythDetails="There are 24 countries worldwide that take in refugees. However, it is undeniable that the US is more politically and economically stable than any of the Arab countries combined. Oppression exists in other Arab countries, the same oppression that forced refugees to leave their own countries in the first place. Additionally, several countries in Europe have welcomed refugees to the best of their ability. In 2019 Europe accepted 123, 920 refugees and have taken even more in past years. However, it is undeniable that the US has more means and resources to take in refugees, and if possible should continue to help to the best of their ability. "
             />
 
             <ContextCard
-              mythTitle="Why can't we just send them help back home?"
+              trueFalse="Confirmed FALSE"
+              mythTitle="It will be easier to send help back home."
               mythSummary=" This kind of help already exists. However, it is simply not enough."
               mythDetails="Organizations like Catholic Relief Services (CRS), UNHCR, and the International Rescue Committee have provided help for countries like Greece, Jordan, Lebanon, Syria, and Iraq. This help includes food, shelter, education, and medical care. This is not enough. The level of oppression and violence that these refugees are experiencing goes beyond resources and virtual support. These people need to leave these countries and resettle so that they and their family members are not injured, imprisoned, or killed.  "
             />
@@ -171,15 +167,73 @@ function Myths() {
           </Col>
           <Col md={6} lg={4}>
             <ContextCard
+              trueFalse="Confirmed FALSE"
               mythTitle="All the refugees that come are adult men."
               mythSummary=" In 2018, a total of 11,099 female refugees, and 11,306 male refugees arrived in the United States."
               mythDetails="Of the male refugees that arrived 5,448 were children, and of the female refugees 5,115 were children. Families never want to split up, that is never the first choice. However, if one family member is able to enter the US they will come and do what they can to bring the rest of their family to safety. These people are beyond desperate they are doing whatever they can for their freedom and for the security of their family. "
             />
 
             <ContextCard
-              mythTitle="The refugee crisis is over."
-              mythSummary=" This statement is simply false. There is still refugee crisis going on in thie world and it needs more attention."
+              trueFalse="Confirmed FALSE"
+              mythTitle="Refugees do not pay taxes, and negatively impact the economy."
+              mythSummary=" Like any other United States citizen refugees are required to pay the same employment, property, sales, and other taxes."
+              mythDetails="The US does not provide refugees with money when they arrive. They are required to work for their income and pay taxes. However, there are some benefits available for emergency situations both medical and financial. In order to receive these benefits refugees must apply and be accepted and they must meet the minimum income and resource  requirements to qualify. On the other hand, they are not allowed to vote. "
+            />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
+  function FactCheckTrue() {
+    return (
+      <Container >
+        <Row>
+          <Col md={6} lg={4}>
+            <ContextCard
+              trueFalse="Confirmed TRUE"
+              mythTitle="Refugees are forced to leave their homes to escape violence."
+              mythSummary=" This is true and it is actually the definition of a refugee."
+              mythDetails=""
+            />
+
+            <ContextCard
+              trueFalse="Confirmed TRUE"
+              mythTitle="Refugees come from countries all over the world."
+              mythSummary=" Although there is a large number of arab refugees, refugees come from all other the world."
+              mythDetails=""
+            />
+
+          </Col>
+          <Col md={6} lg={4}>
+            <ContextCard
+              trueFalse="Confirmed TRUE"
+              mythTitle="Refugees go through a 20 step vetting process."
+              mythSummary=" This statement is entirely true. In fact refugees are the most vetted people in the world."
+              mythDetails=" There is an extensive vetting process that usually takes up to two years. This process consists of an average of 18-20  different steps that refugees must go through to come into the US. Departments that are involved in this process include the FBI, Department of Homeland Security, the National Counterterrorism Center, and much more. Certain refugees like Syrians have a longer and more extensive process, and are the most vetted refugees in the world. Learn more about the details of the vetting process here. "
+            />
+
+            <ContextCard
+              trueFalse="Confirmed TRUE"
+              mythTitle="Refugees contribute their talent, skills, and culture."
+              mythSummary=" Refugees come to their host countries to live a complete and fullfilled life. They do what they can to contribute."
+              mythDetails=""
+            />
+
+          </Col>
+          <Col md={6} lg={4}>
+            <ContextCard
+              trueFalse="Confirmed TRUE"
+              mythTitle="The refugee crisis is still happening."
+              mythSummary=" This statement is completely true. There is still refugee crisis going on in thie world and it needs more attention."
               mythDetails="When thinking about the refugee crisis we want to find a way to solve it, end it for good. Unfortunately this is not possible. Refugees flee their homes because threats on their life or their safety. Therefore, as long as wars continue people will continue to flee them. "
+            />
+
+            <ContextCard
+              trueFalse="Confirmed TRUE"
+              mythTitle="The refugee crisis is not a new phenomenon in history."
+              mythSummary="There has been many refugee crisis' over history. As long as there is war there will be refugees."
+              mythDetails=""
             />
           </Col>
         </Row>
@@ -217,7 +271,14 @@ function Myths() {
               <p>Below is a list of common misconceptions about topics concerning refugees.
                 There is extreme danger in perpetuating false information especially when the lives of inncocent people relies on it. To learn more, click on each myth and debunk it.</p>
             </Container>
-            <FactCheck />
+            <Container>
+              <Typography component="h5" variant="h5" color="error">CONFIRMED FALSE</Typography>
+            </Container>
+            <FactCheckFalse />
+            <Container>
+              <Typography component="h5" variant="h5" className={classes.testingSuccessTheme}>CONFIRMED TRUE</Typography>
+            </Container>
+            <FactCheckTrue />
           </Box>
         </Box>
       </Box>
