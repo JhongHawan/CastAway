bcb0a9c3e6cdd64ee1f5dafa5b1b8b4b26b1a720
Brian Jhong <brianjhong@outlook.com>
2020-04-29 22:12:06 -0700

    Finished templated email portion

--- frontend/src/components/YourRole/YourRole.js
+++ frontend/src/components/YourRole/YourRole.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Divider from './Divider'; 
-import Hero from './Hero';
-import CallToAction from './CallToAction';
-import {useStyles} from './Styles'; 
+import Divider from '../Divider'; 
+import Hero from '../Hero';
+import CallToAction from '../CallToAction';
+import {useStyles} from '../Styles'; 
 import TemplatedEmail from './TemplatedEmail'; 
 import Container from '@material-ui/core/Container'; 
 
-const customStyles = makeStyles(() => ({
-  
+const customStyles = makeStyles(theme => ({
+  heroTop: {
+    margin: theme.spacing(4)
+  }
 })); 
 
 function YourRole() {
+  const customClasses = customStyles(); 
   const classes = useStyles(); 
 
   return (
     <div>
       <Hero
-        showCard={true}
-        sectionTitle={"\"No one puts their children in a boat unless the water is safer than the land\""}
+        showCard={false}
+        sectionTitle='Your Role'
         cardTitle="WHY DOES IT MATTER?"
         cardSubtitle="Subtitle Examples"
         cardBody="The only way to describe this situation while emphasizing its urgency is by calling it what it is, a crisis, a refugee crisis. This crisis is an ongoing global issue that requires immediate action from people who care enough to make a difference."
@@ -30,11 +33,11 @@ function YourRole() {
           align="center"
           justify="center"
           spacing={6}
-          className={classes.homeTopSpace}
         >
-          <Grid item xs={12}>
+          <Grid item xs={12}           
+            className={customClasses.heroTop}
+          >
             <Divider title="HOW CAN YOU HELP" />
-            <CallToAction />
           </Grid>
           <Grid item xs={12}>
             <Divider title="MAKE AN ACTION" />
